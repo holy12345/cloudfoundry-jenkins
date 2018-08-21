c77df306d9cfbe2834a97a4b4d1101779ac92925
holy12345 <spring_holy@163.com>
2018-08-21 13:46:17 +0800

    add controller

--- src/main/java/com/example/cloudfoundryjenkins/CloudfoundryJenkinsApplication.java
+++ src/main/java/com/example/cloudfoundryjenkins/CloudfoundryJenkinsApplication.java
@@ -2,11 +2,19 @@ package com.example.cloudfoundryjenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CloudfoundryJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudfoundryJenkinsApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "This is cloundfoundry jenkins...";
 	}
 }
