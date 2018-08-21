58fe4fa21bdb928416fec0590310460e6fa62f82
holy12345 <spring_holy@163.com>
2018-08-21 12:54:00 +0800

    init

---
+++ src/main/java/com/example/cloudfoundryjenkins/CloudfoundryJenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.example.cloudfoundryjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudfoundryJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudfoundryJenkinsApplication.class, args);
+	}
+}
